e0b0f507a81fd68c4c6c1d328891c6284f9d0239
Ponraj D <ponrajsdr@gmail.com>
2026-08-01 14:20:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (73.19%) - LeetSync

---
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int flip=start^goal;
+        int count=0;
+        for(int i=0;i<32;i++){
+            if(((flip>>i)&1)==1) count++;
+        }
+        return count;
+    }
+}
