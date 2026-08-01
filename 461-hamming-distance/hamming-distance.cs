845abd5d03ad40748a55d4c288e8aa22e7dbe4d4
Ponraj D <ponrajsdr@gmail.com>
2026-08-01 12:06:45 +0530

    Time: 0 ms (100.00%) | Memory: 29.1 MB (22.50%) - LeetSync

--- 461-hamming-distance/hamming-distance.cs
+++ 461-hamming-distance/hamming-distance.cs
@@ -3,7 +3,7 @@ public class Solution {
         int p=x^y;
         int count=0;
         string s=Convert.ToString(p,2);
-        foreach(char c in s.ToArray()){
+        foreach(char c in s){
             if(c=='1') count++;
         }
         return count;
