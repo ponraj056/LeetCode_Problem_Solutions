11228ee881d1b13fe74c060dcc16548590337a91
Ponraj D <ponrajsdr@gmail.com>
2026-08-01 16:00:03 +0530

    Time: 0 ms (100.00%) | Memory: 28.9 MB (71.06%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int HammingWeight(int n) {
+        int count=0;
+        for(int i=0;i<32;i++) {
+            if(((n>>i)&1)==1){
+            count++;
+            } 
+            }
+        return count;
+    }
+}
