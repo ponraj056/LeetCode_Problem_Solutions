5fc5860097f66d3e8ea85f02c44a2356b0f30bf4
Ponraj D <ponrajsdr@gmail.com>
2026-08-06 15:52:02 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (62.90%) - LeetSync

--- 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.js
+++ 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.js
@@ -6,9 +6,7 @@ var sumOddLengthSubarrays = function(arr) {
     let sum=0;
     let n=arr.length;
     for(let i=0;i<n;i++){
-        let t=(i+1)*(n-i);
-        let odd=Math.floor((t+1)/2);
-        sum+=arr[i]*odd;
+        sum+=arr[i]*Math.floor(((((i+1)*(n-i))+1)/2));
     }
     return sum;
 };
