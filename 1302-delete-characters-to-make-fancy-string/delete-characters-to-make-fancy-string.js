ae6a6f355f48333be5a6000c0ea236dcf30e034a
Ponraj D <ponrajsdr@gmail.com>
2026-08-07 12:13:45 +0530

    Time: 95 ms (9.78%) | Memory: 81 MB (11.96%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.js
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.js
@@ -3,14 +3,15 @@
  * @return {string}
  */
 var makeFancyString = function(s) {
-    let res="";
+    let res=[];
     for(let i=0;i<s.length-1;i++){
         if(s[i]===s[i+1] && s[i+1] === s[i+2]){
           continue;
         }
         else{
-            res+=s[i];
+            res.push(s[i]);
         }
     }
-    return res+s[s.length-1];
+    res.push(s[s.length-1]);
+    return res.join("");
 };
