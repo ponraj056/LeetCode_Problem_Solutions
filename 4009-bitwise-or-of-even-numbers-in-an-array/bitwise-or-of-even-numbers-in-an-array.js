7a5afa0be3eee858ecacb181f91b8b1e5bf17fcc
Ponraj D <ponrajsdr@gmail.com>
2026-08-10 12:26:46 +0530

    Time: 2 ms (25.40%) | Memory: 55.4 MB (68.25%) - LeetSync

--- 4009-bitwise-or-of-even-numbers-in-an-array/bitwise-or-of-even-numbers-in-an-array.js
+++ 4009-bitwise-or-of-even-numbers-in-an-array/bitwise-or-of-even-numbers-in-an-array.js
@@ -4,9 +4,9 @@
  */
 var evenNumberBitwiseORs = function(nums) {
     let sum=0;
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]%2==0){
-            sum |=nums[i];
+    for(const i of nums){
+        if(i%2==0){
+            sum |=i;
         }
     }
     return sum;
