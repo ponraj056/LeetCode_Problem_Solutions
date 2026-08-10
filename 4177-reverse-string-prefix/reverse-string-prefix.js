27a53e258575fa65a0cdfbef5e06e4ed7bf9938b
Ponraj D <ponrajsdr@gmail.com>
2026-08-10 14:39:31 +0530

    Time: 3 ms (11.07%) | Memory: 56 MB (55.33%) - LeetSync

---
+++ 4177-reverse-string-prefix/reverse-string-prefix.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reversePrefix = function(s, k) {
+    let arr=[];
+    for(let i=k-1;i>=0;i--){
+       arr.push(s[i]);
+    }
+    for(let i=k;i<s.length;i++){
+        arr.push(s[i]);
+    }
+    return arr.join("");
+};
