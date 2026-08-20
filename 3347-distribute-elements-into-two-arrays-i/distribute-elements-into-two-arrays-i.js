6710bce99965bece3a71a4ab3efb7250e3d70ae7
Ponraj D <ponrajsdr@gmail.com>
2026-08-20 14:10:53 +0530

    Time: 2 ms (50.00%) | Memory: 58.1 MB (18.75%) - LeetSync

---
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var resultArray = function(nums) {
+    let arr1=new Array();
+    let arr2=new Array();
+    arr1.push(nums[0]);
+    arr2.push(nums[1]);
+    for(let i=2;i<nums.length;i++){
+        if(arr1[arr1.length-1]>arr2[arr2.length-1]){
+            arr1.push(nums[i]);
+        }
+        else{
+            arr2.push(nums[i]);
+        }
+    }
+
+    return [...arr1,...arr2];
+};
