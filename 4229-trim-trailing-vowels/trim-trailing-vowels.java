4fb0bcfd7dd74fc3970396f9a77b0b015020b698
Ponraj D <ponrajsdr@gmail.com>
2026-08-27 12:20:51 +0530

    Time: 1 ms (99.05%) | Memory: 44.3 MB (69.61%) - LeetSync

---
+++ 4229-trim-trailing-vowels/trim-trailing-vowels.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    public String trimTrailingVowels(String s) {
+
+        int i = s.length() - 1;
+
+        while (i >= 0 && isVowel(s.charAt(i))) {
+            i--;
+        }
+
+        return s.substring(0, i + 1);
+    }
+
+    public static boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' ||
+                c == 'o' || c == 'u';
+    }
+}
