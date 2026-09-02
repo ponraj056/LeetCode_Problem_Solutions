9b8f9771ddcd80f04c0a82ec497ca92de1d47bce
Ponraj D <ponrajsdr@gmail.com>
2026-09-02 16:26:36 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (65.00%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def uniformArray(self, nums1):
+        # if len(nums1)==1:
+        #     return True
+        # nums2=[nums1[0]-nums1[1]]
+        # nums2+=[nums1[1]]
+        # a=[]
+        # return nums2
+        # for i in range(len(nums2)):
+        #     if nums2[i]%2!=0:
+        #         a.append("odd")
+        #     else:
+        #         a.append("even")
+        # if a.count("odd")==len(a):
+        #     return True 
+        # elif a.count("even")==len(a):
+        #     return True
+        # else:
+        #     return False    
+        return True
